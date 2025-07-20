7026e1b54472fc0ebc1573f8a427c12517a8cb4b
Abay <parsifal@MacBook-Pro-Abay.local>
2025-07-20 14:28:10 +0500

    Added new spring project

---
+++ src/main/java/practice/billInfo/BillInfoApplication.java
@@ -0,0 +1,13 @@
+package practice.billInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/billInfo/BillInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.billInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
